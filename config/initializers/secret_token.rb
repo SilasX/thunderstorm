4f6a2fffcf4f666e3c19b0a83d29990b74e3d9fa
Silas Barta <sbarta@gmail.com>
2012-03-03 23:05:44 -0800

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thunderstorm::Application.config.secret_token = 'cc356360a83768483ac1bfeb2dd5cb6ae71b1925e971c0850151130dba4374c5afada1a645b1af63258966e8ddfd551206571b976a095fd5f3c256592bbe0402'
